6b7db8540d2713241062cf796c34c9b12d8d439c
sinha-aashishkr <112826810+sinha-aashishkr@users.noreply.github.com>
2026-02-21 19:43:07 +0530

    update

--- terraform/modules/gke/main.tf
+++ terraform/modules/gke/main.tf
@@ -1,10 +1,12 @@
+# 1. The GKE Cluster (Control Plane)
 resource "google_container_cluster" "cluster" {
   name     = "${var.env}-gke"
-  location = var.region
+  location = var.region # Regional for High Availability
 
   network    = var.vpc
   subnetwork = var.private_subnet
 
+  # Best Practice: Delete the default pool to use a custom one below
   remove_default_node_pool = true
   initial_node_count       = 1
 
@@ -24,11 +26,17 @@ resource "google_container_cluster" "cluster" {
   ip_allocation_policy {}
 }
 
+# 2. The Zonal Node Pool (Workers)
 resource "google_container_node_pool" "nodes" {
   name       = "${var.env}-nodepool"
-  cluster    = google_container_cluster.cluster.name
+  cluster    = google_container_cluster.cluster.id
   location   = var.region
-  node_count = 2
+
+  # CRITICAL: This restricts nodes to ONE zone only.
+  node_locations = ["${var.region}-a"]
+
+  # Total Nodes = node_count (1) * node_locations (1) = 1 Node total
+  node_count = 1
 
   node_config {
     machine_type    = "e2-medium"
